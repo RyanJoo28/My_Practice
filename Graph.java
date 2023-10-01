32eb37280af521134587c4b4337bf87aa2ac2419
RyanJoo28 <108234255+RyanJoo28@users.noreply.github.com>
2023-10-01 19:31:56 +0800

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,13 +1,10 @@
 package Data_Sttructure_and_Algorithm.Graph;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Graph {
     public static void main(String[] args) {
-        
+
     }
 }
 
@@ -178,5 +175,34 @@ class Vertex {
 
     public Vertex(int val) {
         this.val = val;
+    }
+}
+
+class GraphBFS {
+    /* 广度优先遍历 BFS */
+// 使用邻接表来表示图，以便获取指定顶点的所有邻接顶点
+    List<Vertex> graphBFS(GraphAdjList graph, Vertex startVet) {
+        // 顶点遍历序列
+        List<Vertex> res = new ArrayList<>();
+        // 哈希表，用于记录已被访问过的顶点
+        Set<Vertex> visited = new HashSet<>();
+        visited.add(startVet);
+        // 队列用于实现 BFS
+        Queue<Vertex> que = new LinkedList<>();
+        que.offer(startVet);
+        // 以顶点 vet 为起点，循环直至访问完所有顶点
+        while (!que.isEmpty()) {
+            Vertex vet = que.poll(); // 队首顶点出队
+            res.add(vet);            // 记录访问顶点
+            // 遍历该顶点的所有邻接顶点
+            for (Vertex adjVet : graph.adjList.get(vet)) {
+                if (visited.contains(adjVet))
+                    continue;        // 跳过已被访问过的顶点
+                que.offer(adjVet);   // 只入队未访问的顶点
+                visited.add(adjVet); // 标记该顶点已被访问
+            }
+        }
+        // 返回顶点遍历序列
+        return res;
     }
 }
