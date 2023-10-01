541b6762d8ca623671525a9bda3ca121c0c015a9
RyanJoo28 <108234255+RyanJoo28@users.noreply.github.com>
2023-10-01 19:08:27 +0800

    更新 Graph.java

--- Graph.java
+++ Graph.java
@@ -10,101 +10,102 @@ public class Graph {
     }
 }
 
-/* 基于邻接矩阵实现的无向图类 */
 class GraphAdjMat {
-    List<Integer> vertices; // 顶点列表，元素代表“顶点值”，索引代表“顶点索引”
-    List<List<Integer>> adjMat; // 邻接矩阵，行列索引对应“顶点索引”
+    List<Integer> vertices; // List of vertices, elements represent vertex values, indices represent vertex indices
+    List<List<Integer>> adjMat; // Adjacency matrix, rows and columns correspond to vertex indices
 
-    /* 构造方法 */
+    /* Constructor */
     public GraphAdjMat(int[] vertices, int[][] edges) {
         this.vertices = new ArrayList<>();
         this.adjMat = new ArrayList<>();
-        // 添加顶点
+
+        // Add vertices
         for (int val : vertices) {
             addVertex(val);
         }
-        // 添加边
-        // 请注意，edges 元素代表顶点索引，即对应 vertices 元素索引
+
+        // Add edges
         for (int[] e : edges) {
             addEdge(e[0], e[1]);
         }
     }
 
-    /* 获取顶点数量 */
+    /* Get the number of vertices */
     public int size() {
         return vertices.size();
     }
 
-    /* 添加顶点 */
+    /* Add a vertex */
     public void addVertex(int val) {
         int n = size();
-        // 向顶点列表中添加新顶点的值
         vertices.add(val);
-        // 在邻接矩阵中添加一行
+
+        // Add a new row to the adjacency matrix
         List<Integer> newRow = new ArrayList<>(n);
         for (int j = 0; j < n; j++) {
             newRow.add(0);
         }
         adjMat.add(newRow);
-        // 在邻接矩阵中添加一列
+
+        // Add a new column to the adjacency matrix
         for (List<Integer> row : adjMat) {
             row.add(0);
         }
     }
 
-    /* 删除顶点 */
+    /* Remove a vertex */
     public void removeVertex(int index) {
-        if (index >= size())
+        if (index >= size()) {
             throw new IndexOutOfBoundsException();
-        // 在顶点列表中移除索引 index 的顶点
+        }
+
         vertices.remove(index);
-        // 在邻接矩阵中删除索引 index 的行
         adjMat.remove(index);
-        // 在邻接矩阵中删除索引 index 的列
+
         for (List<Integer> row : adjMat) {
             row.remove(index);
         }
     }
 
-    /* 添加边 */
-    // 参数 i, j 对应 vertices 元素索引
+    /* Add an edge */
     public void addEdge(int i, int j) {
-        // 索引越界与相等处理
-        if (i < 0 || j < 0 || i >= size() || j >= size() || i == j)
+        if (i < 0 || j < 0 || i >= size() || j >= size() || i == j) {
             throw new IndexOutOfBoundsException();
-        // 在无向图中，邻接矩阵沿主对角线对称，即满足 (i, j) == (j, i)
+        }
+
         adjMat.get(i).set(j, 1);
         adjMat.get(j).set(i, 1);
     }
 
-    /* 删除边 */
-    // 参数 i, j 对应 vertices 元素索引
+    /* Remove an edge */
     public void removeEdge(int i, int j) {
-        // 索引越界与相等处理
-        if (i < 0 || j < 0 || i >= size() || j >= size() || i == j)
+        if (i < 0 || j < 0 || i >= size() || j >= size() || i == j) {
             throw new IndexOutOfBoundsException();
+        }
+
         adjMat.get(i).set(j, 0);
         adjMat.get(j).set(i, 0);
     }
 
-    /* 打印邻接矩阵 */
+    /* Print the adjacency matrix */
     public void print() {
         System.out.print("Vertex List = ");
         System.out.println(vertices);
         System.out.println("Adjacency Matrix = ");
-        PrintUtil.printMatrix(adjMat);
+        for (List<Integer> row : adjMat) {
+            System.out.println(row);
+        }
     }
 }
 
-/* 基于邻接表实现的无向图类 */
+/* Implementation of GraphAdjList */
 class GraphAdjList {
-    // 邻接表，key: 顶点，value：该顶点的所有邻接顶点
     Map<Vertex, List<Vertex>> adjList;
 
-    /* 构造方法 */
+    /* Constructor */
     public GraphAdjList(Vertex[][] edges) {
         this.adjList = new HashMap<>();
-        // 添加所有顶点和边
+
         for (Vertex[] edge : edges) {
             addVertex(edge[0]);
             addVertex(edge[1]);
@@ -112,57 +113,69 @@ class GraphAdjList {
         }
     }
 
-    /* 获取顶点数量 */
+    /* Get the number of vertices */
     public int size() {
         return adjList.size();
     }
 
-    /* 添加边 */
+    /* Add an edge */
     public void addEdge(Vertex vet1, Vertex vet2) {
-        if (!adjList.containsKey(vet1) || !adjList.containsKey(vet2) || vet1 == vet2)
+        if (!adjList.containsKey(vet1) || !adjList.containsKey(vet2) || vet1 == vet2) {
             throw new IllegalArgumentException();
-        // 添加边 vet1 - vet2
+        }
+
         adjList.get(vet1).add(vet2);
         adjList.get(vet2).add(vet1);
     }
 
-    /* 删除边 */
+    /* Remove an edge */
     public void removeEdge(Vertex vet1, Vertex vet2) {
-        if (!adjList.containsKey(vet1) || !adjList.containsKey(vet2) || vet1 == vet2)
+        if (!adjList.containsKey(vet1) || !adjList.containsKey(vet2) || vet1 == vet2) {
             throw new IllegalArgumentException();
-        // 删除边 vet1 - vet2
+        }
+
         adjList.get(vet1).remove(vet2);
         adjList.get(vet2).remove(vet1);
     }
 
-    /* 添加顶点 */
+    /* Add a vertex */
     public void addVertex(Vertex vet) {
-        if (adjList.containsKey(vet))
-            return;
-        // 在邻接表中添加一个新链表
-        adjList.put(vet, new ArrayList<>());
+        if (!adjList.containsKey(vet)) {
+            adjList.put(vet, new ArrayList<>());
+        }
     }
 
-    /* 删除顶点 */
+    /* Remove a vertex */
     public void removeVertex(Vertex vet) {
-        if (!adjList.containsKey(vet))
+        if (!adjList.containsKey(vet)) {
             throw new IllegalArgumentException();
-        // 在邻接表中删除顶点 vet 对应的链表
+        }
+
         adjList.remove(vet);
-        // 遍历其他顶点的链表，删除所有包含 vet 的边
+
         for (List<Vertex> list : adjList.values()) {
             list.remove(vet);
         }
     }
 
-    /* 打印邻接表 */
+    /* Print the adjacency list */
     public void print() {
-        System.out.println("邻接表 =");
+        System.out.println("Adjacency List =");
         for (Map.Entry<Vertex, List<Vertex>> pair : adjList.entrySet()) {
             List<Integer> tmp = new ArrayList<>();
-            for (Vertex vertex : pair.getValue())
+            for (Vertex vertex : pair.getValue()) {
                 tmp.add(vertex.val);
+            }
             System.out.println(pair.getKey().val + ": " + tmp + ",");
         }
+    }
+}
+
+/* Vertex class for adjacency list */
+class Vertex {
+    int val;
+
+    public Vertex(int val) {
+        this.val = val;
     }
 }
